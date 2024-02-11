43042f735dca6556906098c8da74db04afc777ca
Muhammad Farjad <m.farjad2004@gmail.com>
2024-02-11 18:47:54 +0500

    introduce CRUD

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,70 +1,74 @@
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Checkbox from '@mui/material/Checkbox';
-import IconButton from '@mui/material/IconButton';
-import CommentIcon from '@mui/icons-material/Comment';
+import List from "@mui/material/List";
+import TodoItem from "./ToDoItem";
+import TodoForm from "./TodoForm";
+import { Box, Typography } from "@mui/material";
 
-const initializeList = [
-    {id: 1, title: 'Go to the gym', completed: false},
-    {id: 2, title: 'Buy groceries', completed: false},
-    {id: 3, title: 'Do the laundry', completed: false},
-    {id: 4, title: 'Clean the house', completed: false},
-]
+import { useState, useEffect } from "react";
 
-function ToDoList(){
+const getInitialData = () => {
+  const data = JSON.parse(localStorage.getItem("todos"));
+  if (!data) return [];
+  return data;
+};
 
-    const [checked, setChecked] = React.useState([0]);
+export default function TodoList() {
+  const [todos, setTodos] = useState(getInitialData);
 
-    const handleToggle = (value) => () => {
-        const currentIndex = checked.indexOf(value);
-        const newChecked = [...checked];
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
-        if (currentIndex === -1) {
-        newChecked.push(value);
+  const removeTodo = (id) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((t) => t.id !== id);
+    });
+  };
+
+  const toggleTodo = (id) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
         } else {
-        newChecked.splice(currentIndex, 1);
+          return todo;
         }
+      });
+    });
+  };
 
-        setChecked(newChecked);
-    };
-
-    return(
-        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-      {initializeList.map((value) => {
-        const labelId = `checkbox-list-label-${value.id}`;
-
-        return (
-          <ListItem
-            key={value.id}
-            secondaryAction={
-              <IconButton edge="end" aria-label="comments">
-                <CommentIcon />
-              </IconButton>
-            }
-            disablePadding
-          >
-            <ListItemButton role={undefined} onClick={handleToggle(value)} dense>
-              <ListItemIcon>
-                <Checkbox
-                  edge="start"
-                  checked={checked.indexOf(value.id) !== -1}
-                  tabIndex={-1}
-                  disableRipple
-                  inputProps={{ 'aria-labelledby': labelId }}
-                />
-              </ListItemIcon>
-              <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
-            </ListItemButton>
-          </ListItem>
-        );
-      })}
-    </List>
-    );
-
+  const addTodo = (text) => {
+    setTodos((prevTodos) => {
+      return [
+        ...prevTodos,
+        { text: text, id: crypto.randomUUID(), completed: false },
+      ];
+    });
+  };
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+        m: 3,
+      }}
+    >
+      <Typography variant="h2" component="h1" sx={{ flexGrow: 1 }}>
+        Todos
+      </Typography>
+      <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+        {todos.map((todo) => (
+          <TodoItem
+            todo={todo}
+            key={todo.id}
+            remove={removeTodo}
+            toggle={() => toggleTodo(todo.id)}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </List>
+    </Box>
+  );
 }
-
-
 
